dc2f56592f3dec5fe685bac00812fc3f817abc09
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2022-02-05 04:55:50 +0200

    🍀 usage command

--- src/bot.ts
+++ src/bot.ts
@@ -1,25 +1,36 @@
+import 'dotenv/config';
 import { Telegraf } from 'telegraf';
 import dayjs from 'dayjs';
 import { hit as hitCounter, get as getUsage } from 'countapi-js';
+import pluralize from 'pluralize';
 
-const token = process.env.BOT_TOKEN;
-const webhookURL = process.env.WEBHOOK_URL;
+const token = process.env.BOT_TOKEN as string;
+const webhookURL = process.env.WEBHOOK_URL as string;
+const namespace = process.env.NAMESPACE as string;
 const port = Number(process.env.PORT || 4006);
 
-const bot = new Telegraf(token as string);
+const bot = new Telegraf(token);
 
 bot.start((ctx) => {
-  ctx.reply(`👋🏻 Welcome!\nForward me a message and I'll tell you when it was sent 👀`);
+  ctx.reply(
+    `👋🏻 Welcome!\nForward me a message and I'll tell you when it was sent 👀`,
+  );
+});
+
+bot.command('usage', async (ctx) => {
+  const { value, status } = await getUsage(namespace, 'usage');
+
+  if (status === 200)
+    ctx.reply(`${value} ${pluralize('message', value)} reached so far!`);
+  else ctx.reply('Network or countapi problems');
 });
 
 bot.on('forward_date', async (ctx) => {
   if (ctx.message.forward_date) {
-    await hitCounter('message-date-bot', 'usage');
+    await hitCounter(namespace, 'usage');
 
     ctx.reply(
-      dayjs(ctx.message.forward_date * 1000).format(
-        'h:mm:ss A\nD MMMM YYYY',
-      ),
+      dayjs(ctx.message.forward_date * 1000).format('h:mm:ss A\nD MMMM YYYY'),
       { reply_to_message_id: ctx.message.message_id },
     );
   } else {
