faa60178f22ac74bd0b53aad7968299401a76ca7
Bogdan Bryzh <bogdan.bryzh@gmail.com>
2022-02-05 04:05:39 +0200

    ✨ bot mvp

---
+++ src/bot.ts
@@ -0,0 +1,45 @@
+import { Telegraf } from 'telegraf';
+import dayjs from 'dayjs';
+import { hit as hitCounter, get as getUsage } from 'countapi-js';
+
+const token = process.env.BOT_TOKEN;
+const webhookURL = process.env.WEBHOOK_URL;
+const port = Number(process.env.PORT || 4006);
+
+const bot = new Telegraf(token as string);
+
+bot.start((ctx) => {
+  ctx.reply(`👋🏻 Welcome!
+  Forward me a message and I'll tell you when it was sent 👀`);
+});
+
+bot.on('forward_date', async (ctx) => {
+  if (ctx.message.forward_date) {
+    await hitCounter('message-date-bot', 'usage');
+
+    ctx.reply(
+      dayjs(ctx.message.forward_date * 1000).format(
+        'h:mm:ss A\nD MMMM YYYY',
+      ),
+      { reply_to_message_id: ctx.message.message_id },
+    );
+  } else {
+    ctx.reply('huh, ping @bogdanbpeterson');
+    console.log(ctx);
+  }
+});
+
+bot.on('message', (ctx) => {
+  ctx.reply('Please forward, not send 🙃');
+});
+
+bot.launch({
+  webhook: {
+    domain: webhookURL,
+    port,
+  },
+});
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
